2e87e1db5e502d10dc8e7531175727a92aef387b
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-01 19:02:07 +0100

    Replace the Pixel class in favor of the Apple class.

---
+++ Snake-Sharp/Apple.cs
@@ -0,0 +1,15 @@
+﻿class Apple
+{
+    public Apple(Position position)
+    {
+        Position = position;
+    }
+
+    public Position Position { get; }
+
+    public void Render()
+    {
+        Console.SetCursorPosition(Position.Left, Position.Top);
+        Console.Write("🍏");
+    }
+}
--- Snake-Sharp/Pixel.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Pixel
-{
-    public int xPos { get; set; }
-    public int yPos { get; set; }
-    public ConsoleColor schermKleur { get; set; }
-    public string karacter { get; set; }
-}
